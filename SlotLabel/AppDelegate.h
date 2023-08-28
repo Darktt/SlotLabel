96fe0d5eb40f302d46803c34cdf1a99b56c2b42d
Eden <pkh_eden@photons.com.tw>
2023-08-28 10:11:48 +0800

    Initial Commit

---
+++ SlotLabel/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  SlotLabel
+//
+//  Created by Eden on 2023/8/28.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ SlotLabel/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  SlotLabel
+//
+//  Created by Eden on 2023/8/28.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
